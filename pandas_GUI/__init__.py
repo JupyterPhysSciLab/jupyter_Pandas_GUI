58653275517546c7d933ed3a4bb4180f4265d1d4
Jonathan Gutow <gutow@uwosh.edu>
2021-06-22 15:54:58 -0500

    refactoring

--- pandas_GUI/__init__.py
+++ pandas_GUI/__init__.py
@@ -1,4 +1,4 @@
-name = "new_pandas_column_GUI"
+name = "pandas_GUI"
 """
 This code utilizes Jupyter widgets to provide a python expression composer GUI
 to generate code to add a new column to an existing Pandas DataFrame.
@@ -12,4 +12,4 @@ mydir = os.path.dirname(__file__)
 
 # load any supporting files
 
-from new_pandas_column_GUI.new_pandas_column_GUI import new_pandas_column_GUI
+from pandas_GUI.new_pandas_column_GUI import new_pandas_column_GUI
