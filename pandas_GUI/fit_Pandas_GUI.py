a31c3fdf9f90316c9df1b1eb4fa39f98d45d2be6
gutow <gutow@uwosh.edu>
2024-03-28 16:44:24 -0500

    remove completed TODO

--- pandas_GUI/fit_Pandas_GUI.py
+++ pandas_GUI/fit_Pandas_GUI.py
@@ -654,9 +654,7 @@ def fit_pandas_GUI(df_info=None, show_text_col = False, **kwargs):
     mirror_ticks = Checkbox(value = False,
                             description = 'Mirror Tick Marks',
                             disabled = True)
-    # TODO: Add aspect ratio and size options (auto, 16:9, 5:3, 7:5, 4:3, 10:8,
-    #  1:1) sizes tiny (300), small (450), medium (800), large (width = 1200),
-    #  huge (2400)
+
     def aspect_change(change):
         if change['new'] != 'auto':
             plot_size.disabled=False
