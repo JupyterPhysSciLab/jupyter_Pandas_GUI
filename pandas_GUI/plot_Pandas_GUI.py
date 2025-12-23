44b4d137f044502c3539895f03a36e21c6c48a2c
gutow <gutow@uwosh.edu>
2025-12-22 19:50:35 -0600

    remove .show in favor of display() fixes issue 24

--- pandas_GUI/plot_Pandas_GUI.py
+++ pandas_GUI/plot_Pandas_GUI.py
@@ -681,8 +681,7 @@ def plot_pandas_GUI(df_info=None, show_text_col = False, **kwargs):
                 text += str(plot_width)+'), height=int('
                 text += str(plot_height)+'))\n'
             step3str += text
-            text = 'display('+figname + '.show(config = {' \
-                    '\'toImageButtonOptions\': {\'format\': \'svg\'}}))'
+            text = 'display('+figname + ')'
             if JPSLUtils.notebookenv == 'NBClassic':
                 replace_text_of_next_cell(
                     importstr+step1str+step2str+step3str+text)
